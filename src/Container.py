9a7191c273749ba9f73886edc304417676a387c9
Oskar Felix Kupke <oskarfelix.kupke@gt-arc.com>
2023-02-24 15:39:05 +0100

    refactored Container class; added some more prints and another sample action to the SampleAgent class

--- src/Container.py
+++ src/Container.py
@@ -16,6 +16,12 @@ class Container(BaseModel):
     actions: Dict[str, List[AbstractAgent]] = {}
     channels: Dict[str, List[AbstractAgent]] = {}
 
+    def set_image(self, **image_params: dict):
+        """
+        Set this container's image to a new image with the specified parameters.
+        """
+        self.image = ImageDescription(**image_params)
+
     def get_agent(self, agent_id: str) -> AbstractAgent:
         """
         Get the agent with the specified agent_id.
@@ -26,32 +32,42 @@ class Container(BaseModel):
 
     def add_agent(self, agent: AbstractAgent):
         """
-        Add the agent to this container, and adds its actions to this container's
-        actions dict for quick access
+        Add the agent to this container.
         """
+        if agent.agent_id in self.agents:
+            return
         self.agents[agent.agent_id] = agent
         agent.container = self
+        self.add_agent_actions(agent)
+
+    def remove_agent(self, agent_id: str):
+        """
+        Remove the agent from the container.
+        """
+        if agent_id not in self.agents:
+            return
+        agent = self.agents[agent_id]
+        self.remove_agent_actions(agent)
+        del self.agents[agent_id]
+
+    def add_agent_actions(self, agent: AbstractAgent):
+        """
+        Add an agent's actions to this container's actions dict for quick access.
+        """
         for action in agent.actions.values():
             if action['name'] not in self.actions:
                 self.actions[action['name']] = []
             if agent not in self.actions[action['name']]:
                 self.actions[action['name']].append(agent)
 
-    def set_image(self, **image_params: dict):
+    def remove_agent_actions(self, agent: AbstractAgent):
         """
-        Set this container's image to a new image with the specified parameters.
+        Remove agent's actions from actions dict.
         """
-        self.image = ImageDescription(**image_params)
-
-    def remove_agent(self, agent_id: str):
-        """
-        Remove the agent from the container.
-        """
-        if agent_id in self.agents:
-            agent = self.agents[agent_id]
-            del self.agents[agent_id]
-            for action in agent.actions.values():
-                self.actions[action['name']].remove(agent)
+        for action in agent.actions.values():
+            if action['name'] not in self.actions:
+                continue
+            self.actions[action['name']].remove(agent)
 
     def invoke_action(self, name: str, parameters: Dict[str, str]) -> str:
         """
--- src/SampleAgent.py
+++ src/SampleAgent.py
@@ -4,6 +4,10 @@ from Models import Message
 
 
 class SampleAgent(AbstractAgent):
+    """
+    Sample Agent class that inherits from AbstractAgent.
+    Prints some information when receiving messages or executing actions.
+    """
 
     def __init__(self, **kwargs):
         super(SampleAgent, self).__init__(**kwargs)
@@ -13,9 +17,22 @@ class SampleAgent(AbstractAgent):
             parameters={'param1': 'String', 'param2': 'Int'},
             result='String'
         )
+        self.add_action(
+            name='add',
+            action=self.add,
+            parameters={'x': 'Int', 'y': 'Int'},
+            result='Int'
+        )
 
     def sample_action_1(self, param1: str, param2: int) -> str:
-        print(f'{self.agent_id} executed sampleAction1 with params: {param1}, {param2}')
+        return f'{self.agent_id} executed sampleAction1 with params: {param1}, {param2}'
+
+    def add(self, x: int, y: int) -> int:
+        print(f'{self.agent_id} executed add with params: {x}, {y}')
+        try:
+            return int(x) + int(y)
+        except ValueError:
+            return 0
 
     def receive_message(self, message: Message):
         super().receive_message(message)
