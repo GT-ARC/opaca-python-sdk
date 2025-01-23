e1d44f05367ad5cc9ab82deb876db54b505ccade
Oskar Felix Kupke <oskarfelix.kupke@gt-arc.com>
2025-01-23 15:47:49 +0100

    added parameter items model class + a new utility function for creating array parameters more easily

--- src/opaca_py/models.py
+++ src/opaca_py/models.py
@@ -9,8 +9,21 @@ class Message(BaseModel):
 
 
 class Parameter(BaseModel):
+
+    class Items(BaseModel):
+        type: str
+        items: Optional['Parameter.Items'] = None
+
+    @staticmethod
+    def list_of(type: str, required: bool = True) -> 'Parameter':
+        """
+        Utility function for making a shallow array parameter.
+        """
+        return Parameter(type='array', required=required, items=Parameter.Items(type=type))
+
     type: str
     required: bool = True
+    items: Optional[Items] = None
 
 
 class ActionDescription(BaseModel):
