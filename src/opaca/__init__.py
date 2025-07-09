7ae8ff97baf1b089bb5630610460a41f4322fbeb
Oskar Felix Kupke <OskarFelix.Kupke@gt-arc.com>
2025-07-09 21:29:08 +0200

    host+port as args; some docs

--- src/opaca/__init__.py
+++ src/opaca/__init__.py
@@ -11,12 +11,30 @@ from .models import (Parameter,
                      Message)
 
 
-def run(title: str, container: Container, app: FastAPI | None = None) -> None:
+def run(title: str,
+        container: Container,
+        host: str | None = None,
+        port: int | None = None,
+        app: FastAPI | None = None,
+    ) -> None:
     """
     Run the container with uvicorn.
+
+    :param title: The title of the application.
+    :param container: The agent container to run the application with.
+    :param host: Optional. The hostname to run the application on. Default to '0.0.0.0'.
+    :param port: Optional. The port to run the application on. Default to the apiPort specified in the container image.
+    :param app: Optional. The FastAPI object with the routes. If this is provided,
+    the title argument becomes irrelevant. Default to the standard OPACA routes.
     """
+    if host is None:
+        host = '0.0.0.0'
+
+    if port is None:
+        port = container.image.apiPort
+
     if app is None:
         app = create_routes(title, container)
 
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=container.image.apiPort)
+    uvicorn.run(app, host=host, port=port)
